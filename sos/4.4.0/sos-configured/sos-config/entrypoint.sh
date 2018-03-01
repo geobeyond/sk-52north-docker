f433399b139e450995a3459154ae735dc158d73b
Francesco Bartoli <francesco.bartoli@geobeyond.it>
2018-03-01 08:12:43 +0100

    Fix wrong path for log file

--- sos/4.4.0/sos-configured/sos-config/entrypoint.sh
+++ sos/4.4.0/sos-configured/sos-config/entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e
 
-/usr/local/bin/invoke update >> /usr/src/app/invoke.log
+/usr/local/bin/invoke update >> /tmp/invoke.log
 
 source $HOME/.bashrc
 source $HOME/.override_env
 
-/usr/local/bin/invoke updatedb >> /usr/src/app/invoke.log
+/usr/local/bin/invoke updatedb >> /tmp/invoke.log
 
 echo "updatedb task done"
 
